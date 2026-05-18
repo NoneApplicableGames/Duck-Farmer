77c4636fe5c0a530ad6eb785fcf3cdbce35349a5
Duncan Cummings <158111174+NoneApplicableGames@users.noreply.github.com>
2026-05-18 14:28:43 +0100

    Player Animation Handler Implemented
    * Player state machine is now able to recognise when the player has inputted the movement keys and play the appropriate animation.
    *Player will now flip to face left/right correctly.
    * Added additional states for future gameplay interaction.

--- Scripts/Player Scripts/character_controller.gd
+++ Scripts/Player Scripts/character_controller.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+#Initalise as a class so that other classes can access the enums
 class_name CharacterController
 
 ## Class dedicated to handling player character movement and state machine.
@@ -7,18 +8,35 @@ class_name CharacterController
 enum PLAYER_ANIMATION_STATES {IDLE_EMPTY, WALK_EMPTY, IDLE_CHICKEN, WALK_CHICKEN, IDLE_GOOSE, WALK_GOOSE, IDLE_WATER_CAN, WALK_WATER_CAN, IDLE_RAKE, WALK_RAKE}
 enum PLAYER_GAMEPLAY_STATES {EMPTY, CHICKEN, GOOSE, WATER_CAN, RAKE}
 
+#To notifty other objects if state has changed.
+#Arguement should ideally pass the new state to whatever the signal is connected to.
+signal animation_state_changed(new_state: PLAYER_ANIMATION_STATES)
+signal gameplay_state_changed(new_state: PLAYER_GAMEPLAY_STATES) 
+
 #This will probabily require the most testing. Put it at the top just in case.
 @export_category("State Machine")
 
 #the player's initial states in state machines have been made an export var to aid with debugging. Note: consider ingame debugging tools later.
 @export var inital_animation_state : PLAYER_ANIMATION_STATES =PLAYER_ANIMATION_STATES.IDLE_EMPTY
-
+@export var initial_gameplay_state : PLAYER_GAMEPLAY_STATES = PLAYER_GAMEPLAY_STATES.EMPTY
 
 #Player's physics properties
 @export_category("Physics Variables")
 @export var walk_speed : float = 0.0
 @export var run_speed : float = 0.0
 
+#Node references
+@onready var SpriteHandle = %PlayerSpriteHandle
+
+#enums are read as intergers relating to the key of the state that the dictoary associates them with. we need to have these vars as int to store and access the infomation.
+
+var current_animation_state : int
+var current_gameplay_state : int
+
+func _ready() -> void:
+
+	current_animation_state = inital_animation_state
+	animation_state_changed.emit(current_animation_state)
 
 func _physics_process(delta: float) -> void:
 	
@@ -27,11 +45,31 @@ func _physics_process(delta: float) -> void:
 	move_direction.x = Input.get_axis("Move_Left","Move_Right")
 	move_direction.y = Input.get_axis("Move_Up","Move_Down")
 	
+	move_direction = move_direction.normalized()
+	
 	#Running has no cooldown like in a PkMn game. No stamina system needed.
 	if Input.is_action_pressed("Run"):
 		self.velocity= move_direction * run_speed
 	else:
 		self.velocity= move_direction * walk_speed
 	
+	
+	change_animation_state(move_direction)
 	# Dont fucking forget to do this at the end of the physics loop or the player wont move.
 	move_and_slide()
+
+func change_animation_state(move_direction : Vector2):
+	
+	if move_direction != Vector2.ZERO:
+		current_animation_state = PLAYER_ANIMATION_STATES.WALK_EMPTY
+	
+	else:
+		current_animation_state = PLAYER_ANIMATION_STATES.IDLE_EMPTY
+		
+	
+	if move_direction == Vector2.LEFT:
+		%PlayerSpriteHandle.flip_h = true
+	elif move_direction == Vector2.RIGHT:
+		%PlayerSpriteHandle.flip_h = false
+	
+	animation_state_changed.emit(current_animation_state)
--- Scripts/Player Scripts/player_animation_handler.gd
+++ Scripts/Player Scripts/player_animation_handler.gd
@@ -1,4 +1,48 @@
 class_name PlayerAnimationHandler
 extends AnimatedSprite2D
 
+
 ## A class to handle all the animations for the different states the player can be in during gameplay.
+
+var _animation_frames  := preload("res://Resources/Sprite_Frames/Player_Sprite_Frames.tres")
+
+
+@export_category("Animation Handler")
+
+func _change_animation_state(_current_animation: CharacterController.PLAYER_ANIMATION_STATES) -> void:
+	
+	match _current_animation:
+		
+		CharacterController.PLAYER_ANIMATION_STATES.IDLE_EMPTY:
+			self.play("Idle_Empty")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.WALK_EMPTY:
+			self.play("Walk_Empty")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.IDLE_CHICKEN:
+			self.play("Idle_Chicken")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.WALK_CHICKEN:
+			self.play("Walk_Chicken")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.IDLE_GOOSE:
+			self.play("Idle_Goose")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.WALK_GOOSE:
+			self.play("Walk_Goose")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.IDLE_WATER_CAN:
+			self.play("Idle_Water_Can")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.WALK_WATER_CAN:
+			self.play("Walk_Water_Can")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.IDLE_RAKE:
+			self.play("Idle_Rake")
+		
+		CharacterController.PLAYER_ANIMATION_STATES.WALK_RAKE:
+			self.play("Walk_Rake")
+		
+		_:
+			self.play("Idle_Empty")
+			print("ANIMATION ERROR: No animation found for current state. Did you spell it correctly?")
