ae3eee7a80e594781e346d45c6b19036c6aa2302
Duncan Cummings <158111174+NoneApplicableGames@users.noreply.github.com>
2026-06-10 19:22:52 +0100

    Scene Switcher Start

--- Scripts/Menus/title_screen.gd
+++ Scripts/Menus/title_screen.gd
@@ -1,7 +1,14 @@
 class_name title_screen
 extends Control
 
- 
+func _ready() -> void:
+	pass
 
 func _on_start_game_button_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,10 +3,19 @@
 class_name main
 extends Node2D
 
+enum GAME_STATES {START_MENU, DEBUG_LEVEL}
+
 signal change_game_scene(new_scene : PackedScene)
+signal change_game_state(new_state : GAME_STATES)
 
-@export_category("Menus")
+# game scenes
 
-@export_category("Levels")
+var start_menu := preload("res://Scenes/UI/start_menu.tscn").instantiate()
+var settings_menu := preload("res://Scenes/UI/settings_menu.tscn").instantiate()
+var ingame_ui := preload("res://Scenes/UI/ingame_ui_layers.tscn").instantiate()
+var debug_level := preload("res://Scenes/Levels/debug_level.tscn").instantiate()
+var player := preload("res://Scenes/player_character.tscn").instantiate()
+var farm_house := preload("res://Scenes/Levels/farm_house.tscn").instantiate()
 
-@export_category("Actors")
+func _ready() -> void:
+	%UILayer.add_child(start_menu)
