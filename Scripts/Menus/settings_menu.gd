f92fd518d1c0e356bec0929acd2bea6e9d8908ec
Duncan Cummings <158111174+NoneApplicableGames@users.noreply.github.com>
2026-06-17 10:58:37 +0100

    Menu Cycling Implemented
    Main is now able to remove and add scenes from the scene tree. In addition we now have UI elements for changing scenes

--- Scripts/Menus/settings_menu.gd
+++ Scripts/Menus/settings_menu.gd
@@ -1,4 +1,4 @@
-class_name title_screen
+class_name SettingsMenu
 extends Control
 
 var main_game
@@ -6,16 +6,10 @@ var current_scene : main.GAME_STATES
 
 signal change_game_state(new_scene : PackedScene)
 
-func _on_start_game_button_pressed() -> void:
-	current_scene = main.GAME_STATES.DEBUG_LEVEL
-	_call_for_scene_change()
 
-func _on_settings_button_pressed() -> void:
-	current_scene = main.GAME_STATES.SETTINGS_MENU
+func _on_back_button_pressed() -> void:
+	current_scene = main.GAME_STATES.START_MENU
 	_call_for_scene_change()
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
 
 func _call_for_scene_change():
 	change_game_state.emit(current_scene)
@@ -24,3 +18,5 @@ func _ready() -> void:
 	# find the main node and connect the change game state to it. This will allow us to emit a signal when the buttons are pressed telling main to change scenes
 	main_game = get_tree().get_first_node_in_group("main")
 	main_game.change_game_state.connect(_call_for_scene_change)
+	
+	print(main_game.change_game_state.get_connections())
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -21,22 +21,27 @@ var farm_house := preload("res://Scenes/Levels/farm_house.tscn").instantiate()
 
 func _ready() -> void:
 	%UILayer.add_child(start_menu)
-	start_menu.change_game_scene.connect(_load_new_scene)
+	start_menu.change_game_state.connect(_load_new_scene)
 
 #function to delete the currently loaded menu/level and spawn the new level depending on the current game state.
 func _load_new_scene(new_scene : GAME_STATES):
-	
-	#find the currently loaded menu and kill it
-	var current_scene := get_tree().get_first_node_in_group("scene")
-	current_scene.queue_free()
+	print(new_scene)
+	#find the currently loaded menu and removes it as a child
+	#we dont queue free since that doesnt allow us to add it back later
+	var current_scene = get_tree().get_first_node_in_group("scene")
+	%UILayer.remove_child(current_scene)
 	
 	#work out what scene needs to be loaded next
 	#add the new scene as a child of main if level, add as child of canvasLayer if Menu/UI element
 	match new_scene:
 		GAME_STATES.START_MENU:
 			%UILayer.add_child(start_menu)
+			start_menu.change_game_state.connect(_load_new_scene)
 		GAME_STATES.SETTINGS_MENU:
 			%UILayer. add_child(settings_menu)
+			settings_menu.change_game_state.connect(_load_new_scene)
 		GAME_STATES.DEBUG_LEVEL:
 			self.add_child(debug_level)
+			#debug_level.change_game_state.connect(_load_new_scene)
+	
 	
